92e7230d7b1de72a504168c02c4951ef5f6c7069
Junior <mjmabunda0@gmail.com>
2024-12-05 12:02:07 +0200

    Add index page and render with flask

---
+++ flaskr/__init__.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+from dotenv import load_dotenv
+import os
+
+
+app = Flask(__name__)
+
+load_dotenv()
+
+params = {
+    'engine': 'google',
+    'api_key': os.environ.get('SERP_API_KEY'),
+    'q': 'weather today'
+}
+
+
+@app.route('/')
+def index():
+    return render_template('index.html')
