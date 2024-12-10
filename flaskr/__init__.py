005ba7600770b71cb998a9a1fbeae9f03c75c8ce
Junior <mjmabunda0@gmail.com>
2024-12-10 14:09:08 +0200

    Add sections for organic results, knowledge graph and pagination

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -5,6 +5,8 @@ from dotenv import load_dotenv
 import json
 import re
 
+from flaskr.storage import save
+
 
 app = Flask(__name__)
 load_dotenv()
@@ -16,17 +18,22 @@ def index():
 
 
 @app.route('/results', methods=['GET', 'POST'])
-def search(q: str = ''):
+def results(q: str = ''):
     if request.method == 'POST':
-        q = request.form.get('search_query')
+        q = request.form.get('search')
         params = {
             'q': q,
             'engine': 'google',
             'api_key': os.environ.get('SERP_API_KEY')
         }
         search = GoogleSearch(params)
+        search_dict = search.get_dict()
+        # Save to a json file
+        save(search_dict)
         
-        results_key = re.search(r'\w*_(results)', " ".join(search.get_dict().keys()))
-        results = search.get_dict()[results_key.group(0)]
+        organic_results = search_dict['organic_results']
+        knowledge_graph = search_dict['knowledge_graph']
         
-    return render_template('results.html', results=results)
+    return render_template('results.html',
+                           organic_results=organic_results,
+                           knowledge_graph=knowledge_graph)
